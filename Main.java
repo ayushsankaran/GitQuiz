ea4f3712e9649fbbebd99524a94108e94ddf7438
ayushsankaran <10017711@sbstudents.org>
2023-12-11 11:54:17 -0500

    main java file added
    this has the code for the main java file

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        InclusiveSum is = (int x, int y) -> {
+            int sum = 0;
+            for(int i = x; i <= y; i++){
+                sum += i;
+            }
+            return sum;
+        };
+        System.out.println(is.sumOfNums(Integer.parseInt(args[0]),Integer.parseInt(args[1])));
+    }
+}
